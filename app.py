fcbaad2cb75858cfcf957494f6afc58a668b8db6
Ofucho <jben8564@gmail.com>
2020-01-09 20:34:13 +0300

    day2

--- app.py
+++ app.py
@@ -1,41 +1,34 @@
-from flask import Flask,render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request
+
 app = Flask(__name__)
 
+import psycopg2
+import pygal
 
 
-
-
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['POST', 'GET'])
 def login_page():
     error = None
     if request.method == 'POST':
         if request.form['username'] != 'admin' or request.form['password'] != '1234':
             error = 'Invalid Credentials. Please try again.'
         else:
-            return redirect(url_for('home'))
+            return redirect(url_for('dashboard'))
     return render_template('login.html', error=error)
 
-@app.route('/dashboard')
-def dashboard():
 
+@app.route('/dashboard', methods=['GET', 'POST'])
+def dashboard():
+    if request.method == 'POST':
+        return redirect(url_for('login_page'))
 
     return render_template('dashboard.html')
 
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    if request.method == 'POST':
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+     return redirect(url_for('login_page'))
 
 
 if __name__ == '__main__':
